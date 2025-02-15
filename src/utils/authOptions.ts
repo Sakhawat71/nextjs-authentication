1ed511fa277b87934d138805bbf4ffa77f9b3301
Sakhawat Showrav <sakhawat.showrav@gmail.com>
2025-02-16 01:59:00 +0600

    Implement GitHub sign-in functionality and refine login page layout

--- src/utils/authOptions.ts
+++ src/utils/authOptions.ts
@@ -1,15 +1,14 @@
 import { NextAuthOptions } from "next-auth"
-// import GithubProvider from "next-auth/providers/github"
+import GithubProvider from "next-auth/providers/github"
 
-export const authOptions : NextAuthOptions = {
+export const authOptions: NextAuthOptions = {
 
     providers: [
 
-        // GithubProvider({
-        //     clientId: process.env.GITHUB_ID,
-        //     clientSecret: process.env.GITHUB_SECRET,
-        // }),
-        
-        
+        GithubProvider({
+            clientId: process.env.GITHUB_ID as string,
+            clientSecret: process.env.GITHUB_SECRET as string,
+        }),
+
     ],
 }
