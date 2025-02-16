c1cfbd3c248625bf0d78bb21745b7b39fb4f6f41
Sakhawat Showrav <sakhawat.showrav@gmail.com>
2025-02-16 14:50:09 +0600

    Update ESLint configuration and clean up unused variable rules in login and register pages

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,5 +1,4 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-/* eslint-disable @typescript-eslint/no-explicit-any */
+
 "use client";
 import { loginUser } from "@/utils/actions/loginUser";
 import { signIn } from "next-auth/react";
