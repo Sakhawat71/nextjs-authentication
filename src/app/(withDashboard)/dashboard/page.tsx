ee7cb4f115cf1cd54d24277da38190d9f23a7227
Sakhawat Showrav <sakhawat.showrav@gmail.com>
2025-02-16 13:23:50 +0600

    dashborad redesign

--- src/app/(withDashboard)/dashboard/page.tsx
+++ src/app/(withDashboard)/dashboard/page.tsx
@@ -9,20 +9,28 @@ const DashboardPage = async () => {
 
 
     return (
-        <div>
-            <h1 className="text-4xl text-center mt-10">Welcome To Dashboard Page</h1>
-            {
-                session?.user && <>
-                    <h3>{session?.user?.name}</h3>
-                    <Image
-                        src={session?.user?.image as string}
-                        width={100}
-                        height={100}
-                        alt="user image"
-                    />
-                    <p>{session?.user?.email}</p>
-                </>
-            }
+        <div className="flex flex-col items-center  min-h-screen bg-gray-100 p-6">
+            <div className="bg-white shadow-lg rounded-lg p-6 w-full max-w-md text-center">
+                <h1 className="text-3xl font-bold text-gray-800 mb-4">
+                    Welcome to Your Dashboard
+                </h1>
+
+                {session?.user && (
+                    <div className="flex flex-col items-center">
+                        <Image
+                            src={session?.user?.image as string}
+                            width={100}
+                            height={100}
+                            alt="User image"
+                            className="rounded-full border-2 border-gray-300 shadow-md"
+                        />
+                        <h3 className="text-xl font-semibold text-gray-700 mt-3">
+                            {session?.user?.name}
+                        </h3>
+                        <p className="text-gray-600">{session?.user?.email}</p>
+                    </div>
+                )}
+            </div>
         </div>
     );
 };
